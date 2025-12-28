a9fca1ad9b5c6aae8515aa2c559f90378d45790f
Paweł Walaszek <pawel.walaszek@eengine.pl>
2025-12-28 13:46:31 +0100

    Poprawienie strefy czasowej

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -21,3 +21,7 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], \FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+$appTimeZone = $_ENV['APP_TIMEZONE'] ?? $_SERVER['APP_TIMEZONE'] ?? 'UTC';
+ini_set('date.timezone', $appTimeZone);
+date_default_timezone_set($appTimeZone);
--- src/Kernel.php
+++ src/Kernel.php
@@ -10,4 +10,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+        $appTimeZone = $_SERVER['APP_TIMEZONE'] ?? $_ENV['APP_TIMEZONE'] ?? 'UTC';
+        ini_set('date.timezone', $appTimeZone);
+        date_default_timezone_set($appTimeZone);
+
+        parent::__construct($environment, $debug);
+    }
 }
